01e2d4294f9a5ba6f1fbbb6b22235b819c04e5cc
tharamalai <s7070054@kmitl.ac.th>
2015-10-26 16:52:09 -0700

    Initial Commit

---
+++ desktop/src/com/sleepygrizzly/core/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.sleepygrizzly.core.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.sleepygrizzly.core.maingame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new maingame(), config);
+	}
+}
