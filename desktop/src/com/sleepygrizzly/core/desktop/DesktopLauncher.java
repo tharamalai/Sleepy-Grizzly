bcafd3f4bc22c9b53069b5f9c087fe23df993a2d
Jessegade <s7070068@kmitl.ac.th>
2015-11-16 11:31:28 +0700

    delete import physic

--- desktop/src/com/sleepygrizzly/core/desktop/DesktopLauncher.java
+++ desktop/src/com/sleepygrizzly/core/desktop/DesktopLauncher.java
@@ -4,11 +4,7 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.sleepygrizzly.core.DrawObject;
 import com.sleepygrizzly.core.HomeGame;
-import com.sleepygrizzly.core.MainGame;
-import com.sleepygrizzly.core.Physics1;
-import com.sleepygrizzly.core.Physics2;
-import com.sleepygrizzly.core.Physics3;
-import com.sleepygrizzly.core.Physics4;
+
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
